e2dd66888ac3188ad937a3b657764ca891c9a30c
harkl <3zy3@protonmail.ch>
2024-10-22 12:04:01 +1300

    add consts for base

--- anchor/programs/memeoor/src/state/constants.rs
+++ anchor/programs/memeoor/src/state/constants.rs
@@ -5,3 +5,6 @@ pub const FEE_VAULT_SEED: &[u8] = b"fee_vault";
 pub const PROOF_ACC_SEED: &[u8] = b"proof_acc";
 pub const TIME_DIFFICULTY_ADJUSTMENT: u32 = 600; // 10 minutes in seconds
 pub const STARTING_DIFFICULTY: u64 = 18_014_398_509_481_984;
+
+pub const BASE_FEE: u64 = 1_500_000; // 0.0015 SOL in lamports
+pub const ATTENTION_ACC: &str = "ATTNY7hEkP4DfP5SML6rtpLgNMdhT8PMLfCxqbRRHVrL";
