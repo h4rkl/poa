16d80a05f0a911ef3832ba36d7ba95b62cb3164d
harkl <3zy3@protonmail.ch>
2024-10-29 15:19:06 +1300

    update token image

--- anchor/tests/test-helpers.ts
+++ anchor/tests/test-helpers.ts
@@ -9,7 +9,7 @@ export const PROOF_ACC_SEED = Buffer.from("proof_acc");
 export const poaFees = new PublicKey("CLiCKaKS3DZUCr9WazTnXSM1Tky7kgrKy6tDQ2tSeZ9P");
 
 export const attentionTokenMetadata = {
-  uri: "https://gist.githubusercontent.com/h4rkl/c7e582319fe6c39570f12c1d77eba31f/raw/bb854d81d3dee64a8cc5dd4970fc61ebd841b603/data.json",
+  uri: "https://gist.githubusercontent.com/h4rkl/c7e582319fe6c39570f12c1d77eba31f/raw/eea1b8700acd394597d75bdc7ee615e05c1a3ffc/data.json",
   name: "Attention",
   symbol: "CLICK",
 };
