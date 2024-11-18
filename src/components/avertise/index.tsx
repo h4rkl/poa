d66e92a532aa999fb1f23061fd688f90da08a72b
harkl <3zy3@protonmail.ch>
2024-11-18 19:03:03 +1300

    style for mobile

--- src/components/avertise/index.tsx
+++ src/components/avertise/index.tsx
@@ -20,14 +20,14 @@ export const Advertisement: React.FC<AdvertisementProps> = ({
   buttonColor,
 }) => {
   return (
-    <div className="flex items-center justify-center pt-6">
-      <div className="w-full max-w-xl">
+    <div className="flex items-center justify-center pt-6 px-1 md:px-0">
+      <div className="w-full max-w-xl md:px-4 pt-12">
         <div
-          className={`flex items-center justify-between px-3 py-2 ${bgColor} rounded-lg shadow-md`}
+          className={`flex flex-row items-center justify-between md:pl-2 pr-2 py-2 ${bgColor} rounded-lg shadow-md`}
         >
-          <div className="flex items-center space-x-4">
+          <div className="flex flex-row items-center space-y-0 space-x-4">
             <div
-              className={`rounded-full overflow-hidden ${textColor}`}
+              className={`hidden md:block rounded-full overflow-hidden ${textColor}`}
               style={{ width: "86px", height: "86px" }}
             >
               <img
@@ -36,15 +36,17 @@ export const Advertisement: React.FC<AdvertisementProps> = ({
                 alt={`${title} logo`}
               />
             </div>
-            <div>
-              <h1 className={`text-3xl font-bold ${textColor}`}>{title}</h1>
-              <p className="text-gray-500 text-lg font-light mt-2">
+            <div className="text-left">
+              <h1 className={`text-2xl md:text-3xl font-bold ${textColor}`}>
+                {title}
+              </h1>
+              <p className="hidden sm:block text-gray-500 text-base text-lg font-light mt-2">
                 {description}
               </p>
             </div>
           </div>
           <button
-            className={`${buttonColor} text-white px-4 py-2 rounded-lg hover:bg-opacity-80`}
+            className={`${buttonColor} text-white px-4 py-2 rounded-lg mt-0 hover:bg-opacity-80 cursor-pointer`}
             onClick={() => window.open(url, "_blank")}
           >
             Find out more
