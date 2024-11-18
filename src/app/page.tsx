6b3a35e8b61e3d8670bfa20eb4d565351a56c093
harkl <3zy3@protonmail.ch>
2024-11-19 11:45:22 +1300

    randomise advert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,20 @@ import { ads } from "@/components/avertise/ads";
 export default function Page() {
   return (
     <div className={styles.container}>
-      {ads.map((ad, index) => (
-        <Advertisement
-          key={index}
-          icon={ad.icon}
-          title={ad.title}
-          description={ad.description}
-          url={ad.url}
-          bgColor={ad.bgColor}
-          textColor={ad.textColor}
-          buttonColor={ad.buttonColor}
-        />
-      ))}
+      {(() => {
+        const ad = ads[Math.floor(Math.random() * ads.length)];
+        return (
+          <Advertisement
+            icon={ad.icon}
+            title={ad.title}
+            description={ad.description}
+            url={ad.url}
+            bgColor={ad.bgColor}
+            textColor={ad.textColor}
+            buttonColor={ad.buttonColor}
+          />
+        );
+      })()}
       <POAFeature />
       <AccountPoa />
       <LeaderFeature />
